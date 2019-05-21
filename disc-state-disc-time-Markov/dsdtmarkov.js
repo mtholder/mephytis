15d4a236cfb1a07759cb21844d2f76807deaa1b9
Mark T. Holder <mtholder@gmail.com>
2019-05-21 14:39:03 -0500

    x axis

--- disc-state-disc-time-Markov/dsdtmarkov.js
+++ disc-state-disc-time-Markov/dsdtmarkov.js
@@ -6,6 +6,7 @@ var num_urns = draw_text.length;
 
 var switch_prob = 7.0/16.0;
 var num_samples_per_click = 1;
+var g_s_hat = '?';
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // slider code from https://bl.ocks.org/johnwalley/e1d256b81e51da68f7feb632a53c3518
@@ -81,19 +82,46 @@ var draw_next_bead = function(sprob) {
         }
     }
     update_data_boxes(global_draws);
+    update_summary_stats(global_draws);
 };
 var clear_data = function() {
     global_draws = [];
     update_data_boxes(global_draws);
+    update_summary_stats(global_draws);
 };
 d3.select(".simbtn")
     .attr("onclick", "draw_next_bead(switch_prob)");
 d3.select(".clearbtn")
     .attr("onclick", "clear_data()");
 
+var update_summary_stats = function(data) {
+    var ns = 0;
+    var nd = 0;
+    var i;
+    if (data.length > 1) {
+        for (i = 1; i < data.length; ++i) {
+            if (data[i] == data[i - 1]) {
+                ns = ns + 1;
+            } else {
+                nd = nd + 1;
+            }
+        }
+    }
+    d3.select("#value-ndiffs").text(nd);
+    d3.select("#value-nsame").text(ns);
+    d3.select("#value-ntransitions").text(ns + nd);
+    if (ns + nd == 0) {
+        g_s_hat = '?';
+        d3.select("#value-estimate-of-s").text(g_s_hat);
+    } else {
+        g_s_hat = nd/(nd + ns);
+        d3.select("#value-estimate-of-s").text(d3.format(".3f")(g_s_hat));
+    }
 
+};
 
 var update_data_boxes = function (data) {
+    d3.select("#samplesize").text(data.length);
     var drawboxes = d3.select(".drawcont")
             .selectAll("span")
             .data(data);
@@ -107,3 +135,41 @@ var update_data_boxes = function (data) {
     drawboxes.exit()
         .remove();
 };
+
+var like_width = 500;
+var like_height = 400;
+
+var like_margin = ({top: 20, right: 30, bottom: 30, left: 50});
+var like_x = d3.scaleLinear()
+        .domain([0.0, 1.0])
+        .range([like_margin.left, like_width - like_margin.right]);
+var like_x_axis = function(el) {
+    el.attr("transform", "translate(0, " + like_height - like_margin.bottom + ")")
+        .call(d3.axisBottom(like_x).ticks(10).tickSizeOuter(0));
+};
+var like_svg = d3.select("#likelihood-trace-div")
+    .append("svg")
+    .attr("width", like_width)
+    .attr("height", like_height);
+like_svg.append("g").call(like_x_axis);
+
+/*
+
+  svg.append("g")
+      .call(xAxis);
+
+  svg.append("g")
+      .call(yAxis);
+
+  svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("d", line);
+
+  return svg.node();
+}
+*/
